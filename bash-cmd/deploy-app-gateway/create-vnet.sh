0a6c3e28f1280ed773c0c5c2dc7f1e55b1a3ce14
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-07-31 15:32:36 +0000

    Create Virtual Network with 2 Subnets

--- bash-cmd/deploy-app-gateway/create-vnet.sh
+++ bash-cmd/deploy-app-gateway/create-vnet.sh
@@ -1,10 +1,11 @@
+#Create Virtual Network with 2 Subnets
 az network vnet create \
 --resource-group RG-LAB22 \
 --name vNET-LAB22 \
 --address-prefix 10.0.0.0/16 \
 --subnet-name Subnet1 \
 --subnet-prefix 10.0.0.0/24 \
-\
+
 az network vnet subnet create \
 --resource-group RG-LAB22 \
 --vnet-name vNET-LAB22 \
