2abea83981330afaca216fe9edae909317d6bb16
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-08-01 20:54:51 +0000

    major changes

--- bash-cmd/deploy-app-gateway/ALL.sh
+++ bash-cmd/deploy-app-gateway/ALL.sh
@@ -10,11 +10,11 @@ az network vnet create \
   --resource-group RG-LAB22 \
   --location eastus \
   --network-security-group "" \
-  --subnet-name Subnet1 \
+  --subnet-name APPGW-Subnet \
   --subnet-prefixes 10.0.0.0/24 \
 
 az network vnet subnet create \
-  --name Subnet2 \
+  --name BE-Subnet \
   --address-prefixes 10.0.1.0/24 \
   --vnet-name vNET-LAB22 \
   --resource-group RG-LAB22 \
@@ -53,6 +53,134 @@ az network nsg rule create \
   --destination-port-ranges 22 \
   --access Allow \
   --priority 100 \
+
+# Create Virtual Machine VM1
+az vm create \
+  --name VM1 \
+  --resource-group RG-LAB22 \
+  --admin-username azureadmin \
+  --admin-password admin123123! \
+  --image UbuntuLTS \
+  --vnet-name vNET-LAB22 \
+  --subnet BE-Subnet \
+  --nsg NSG-VM1 \
+
+# Install Apache Web Service in the Virtual Machine VM1 
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM1 \
+  --resource-group RG-LAB22 \
+  --settings '\{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/omeatai/my-profile-app.git /var/www/html/"\}' \
+
+# Create a Network Security Group For VM2
+az network nsg create \
+  --name NSG-VM2 \
+  --resource-group RG-LAB22 \
+  --location eastus \
+
+# Create Network Security Group Rule AllowHTTPInbound For NSG-VM2
+az network nsg rule create \
+  --name AllowHTTPInbound \
+  --nsg-name NSG-VM2 \
+  --resource-group RG-LAB22 \
+  --protocol Tcp \
+  --direction Inbound \
+  --source-address-prefixes '*' \
+  --source-port-ranges '*' \
+  --destination-address-prefixes 'VirtualNetwork' \
+  --destination-port-ranges 80 \
+  --access Allow \
+  --priority 200 \
+
+# Create Network Security Group Rule AllowSSHInbound For NSG-VM2
+az network nsg rule create \
+  --name AllowSSHInbound \
+  --nsg-name NSG-VM2 \
+  --resource-group RG-LAB22 \
+  --protocol Tcp \
+  --direction Inbound \
+  --source-address-prefixes '*' \
+  --source-port-ranges '*' \
+  --destination-address-prefixes 'VirtualNetwork' \
+  --destination-port-ranges 22 \
+  --access Allow \
+  --priority 100 \	
+
+# Create Virtual Machine VM2
+az vm create \
+  --name VM2 \
+  --resource-group RG-LAB22 \
+  --admin-username azureadmin \
+  --admin-password admin123123! \
+  --image UbuntuLTS \
+  --vnet-name vNET-LAB22 \
+  --subnet BE-Subnet \
+  --nsg NSG-VM2 \
+
+# Install Apache Web Service in the Virtual Machine VM2 (Project Images WebServer)
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM2 \
+  --resource-group RG-LAB22 \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && echo This is my Project IMAGES Webserver > /var/www/html/images/index.html"}' \
+
+# Create a Network Security Group For VM3
+az network nsg create \
+  --name NSG-VM3 \
+  --resource-group RG-LAB22 \
+  --location eastus
+
+# Create Network Security Group Rule AllowHTTPInbound For NSG-VM3
+az network nsg rule create \
+  --name AllowHTTPInbound \
+  --nsg-name NSG-VM3 \
+  --resource-group RG-LAB22 \
+  --protocol Tcp \
+  --direction Inbound \
+  --source-address-prefixes '*' \
+  --source-port-ranges '*' \
+  --destination-address-prefixes 'VirtualNetwork' \
+  --destination-port-ranges 80 \
+  --access Allow \
+  --priority 200 \
+
+# Create Network Security Group Rule AllowSSHInbound For NSG-VM3
+az network nsg rule create \
+  --name AllowSSHInbound \
+  --nsg-name NSG-VM3 \
+  --resource-group RG-LAB22 \
+  --protocol Tcp \
+  --direction Inbound \
+  --source-address-prefixes '*' \
+  --source-port-ranges '*' \
+  --destination-address-prefixes 'VirtualNetwork' \
+  --destination-port-ranges 22 \
+  --access Allow \
+  --priority 100 \	
+
+# Create Virtual Machine VM3 (Project Videos WebServer)
+az vm create \
+  --name VM3 \
+  --resource-group RG-LAB22 \
+  --admin-username azureadmin \
+  --admin-password admin123123! \
+  --image UbuntuLTS \
+  --vnet-name vNET-LAB22 \
+  --subnet BE-Subnet \
+  --nsg NSG-VM3 \
+
+# Install Apache Web Service in the Virtual Machine VM3 (Project Videos WebServer)
+az vm extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --vm-name VM3 \
+  --resource-group RG-LAB22 \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && echo This is my Project VIDEOS WebServer > /var/www/html/videos/index.html"}' \
 
 
 
