0aa47f1da79e6296da6579fe5d9216c1c63a8a10
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-08-04 16:20:33 +0000

    deploy-vnet-peering

---
+++ bash-cmd/deploy-vnet-peering/ALL.sh
@@ -0,0 +1,66 @@
+#Create Resource Group
+az group create \
+  --name RG-LAB22 \
+  --location eastus
+
+az group create --name RG-14-CONNECT-vNETs --location westeurope
+
+az network vnet create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --name vNET-A \
+    --address-prefix 10.0.0.0/16 \
+    --subnet-name Subnet-01-vNET-A \
+    --subnet-prefix 10.0.0.0/24 \
+    --location westeurope
+
+az network vnet create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --name vNET-B \
+    --address-prefix 10.1.0.0/16 \
+    --subnet-name Subnet-01-vNET-B \
+    --subnet-prefix 10.1.0.0/24 \
+    --location westeurope
+
+az network vnet create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --name vNET-C \
+    --address-prefix 10.2.0.0/16 \
+    --subnet-name Subnet-01-vNET-C \
+    --subnet-prefix 10.2.0.0/24 \
+    --location northeurope
+
+az vm create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --no-wait \
+    --name VM-A \
+    --location westeurope \
+    --vnet-name vNET-A \
+    --subnet Subnet-01-vNET-A \
+    --image UbuntuLTS \
+    --size Standard_B1s \
+    --admin-username adminuser \
+    --admin-password adminadmin123!
+
+az vm create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --no-wait \
+    --name VM-B \
+    --location westeurope \
+    --vnet-name vNET-B \
+    --subnet Subnet-01-vNET-B \
+    --image UbuntuLTS \
+    --size Standard_B1s \
+    --admin-username adminuser \
+    --admin-password adminadmin123!
+
+az vm create \
+    --resource-group RG-14-CONNECT-vNETs \
+    --no-wait \
+    --name VM-C \
+    --location northeurope \
+    --vnet-name vNET-C \
+    --subnet Subnet-01-vNET-C \
+    --image UbuntuLTS \
+    --size Standard_B1s \
+    --admin-username adminuser \
+    --admin-password adminadmin123!
