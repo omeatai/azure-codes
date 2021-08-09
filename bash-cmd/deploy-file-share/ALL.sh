9933b6cdf73caa591d7a8f91289dbbbb18c0643c
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-08-09 16:00:01 +0000

    m

--- bash-cmd/deploy-file-share/ALL.sh
+++ bash-cmd/deploy-file-share/ALL.sh
@@ -2,3 +2,12 @@
 az group create \
   --name RG-LAB24 \
   --location eastus
+
+#Create Virtual Network
+az network vnet create \
+    --resource-group RG-LAB24 \
+    --name vNET-LAB24 \
+    --address-prefix 10.0.0.0/16 \
+    --subnet-name mySubnet \
+    --subnet-prefix 10.0.0.0/24 \
+    --location eastus
