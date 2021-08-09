58a64f6df52a2e31229bbf26e7ee42cb16ead4b7
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-08-09 16:01:47 +0000

    m

--- bash-cmd/deploy-file-share/ALL.sh
+++ bash-cmd/deploy-file-share/ALL.sh
@@ -11,3 +11,37 @@ az network vnet create \
     --subnet-name mySubnet \
     --subnet-prefix 10.0.0.0/24 \
     --location eastus
+
+#Create VM-1
+az vm create \
+    --resource-group RG-LAB24 \
+    --no-wait \
+    --name VM-1 \
+    --location eastus \
+    --vnet-name vNET-LAB24 \
+    --subnet mySubnet \
+    --image win2016datacenter \
+    --size Standard_B2s \
+    --admin-username azureadmin \
+    --admin-password admin123123!
+
+#Create VM-2
+az vm create \
+    --resource-group RG-LAB23 \
+    --no-wait \
+    --name VM-2 \
+    --location eastus \
+    --vnet-name vNET-2 \
+    --subnet Subnet-2 \
+    --image UbuntuLTS \
+    --size Standard_B1s \
+    --admin-username azureadmin \
+    --admin-password admin123123!
+
+
+
+
+
+
+
+
